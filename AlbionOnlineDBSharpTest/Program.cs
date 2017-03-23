427899f59bafb1f9c16d8032c6ffd05c0031f62d
Jinivus <jinivus@jinivus.com>
2017-03-23 13:24:54 +1000

    First dev version

---
+++ AlbionOnlineDBSharpTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using AlbionOnlineDB.Models;
+using System;
+using System.Linq;
+
+namespace AlbionOnlineDBSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(Item i in AlbionOnlineDB.Items.GetItems().Result)
+            {
+
+            }
+            Console.WriteLine(String.Join(",", AlbionOnlineDB.Items.GetItems().Result.Select(x=> x.uniquename)));
+            Console.ReadKey();
+        }
+    }
+}
