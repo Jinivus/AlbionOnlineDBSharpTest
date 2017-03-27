995267b73fd3e1cdf633ade42ce11fa4b8855746
Jinivus <jinivus@jinivus.com>
2017-03-27 14:26:20 +1000

    Updated to test across multiple threads

--- AlbionOnlineDBSharpTest/Program.cs
+++ AlbionOnlineDBSharpTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AlbionOnlineDB.Models;
 using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AlbionOnlineDBSharpTest
 {
@@ -8,11 +10,16 @@ namespace AlbionOnlineDBSharpTest
     {
         static void Main(string[] args)
         {
-            foreach(Item i in AlbionOnlineDB.Items.GetItems().Result)
-            {
+            Console.WriteLine("Test1");
+            Parallel.ForEach(AlbionOnlineDB.Items.GetItems().Result.GetRange(0,20), (item) => {
+                Console.WriteLine(item.uniquename + ":" + item.ItemInfo.tier);
+            });
+            Console.WriteLine("Test2");
+            Parallel.ForEach(AlbionOnlineDB.Items.GetItems().Result.GetRange(20, 20), (item) => {
+                Console.WriteLine(item.uniquename + ":" + item.ItemInfo.tier);
+            });
+            Console.WriteLine("Test3");
 
-            }
-            Console.WriteLine(String.Join(",", AlbionOnlineDB.Items.GetItems().Result.Select(x=> x.uniquename)));
             Console.ReadKey();
         }
     }
