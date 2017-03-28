aeda4dfbae31f573b28dc7fbb3c23f7d29923234
Jinivus <jinivus@jinivus.com>
2017-03-28 14:29:01 +1000

    Debug to retrieve different item types based on shopcategory

--- AlbionOnlineDBSharpTest/Program.cs
+++ AlbionOnlineDBSharpTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AlbionOnlineDB.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,15 +11,30 @@ namespace AlbionOnlineDBSharpTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test1");
-            Parallel.ForEach(AlbionOnlineDB.Items.GetItems().Result.GetRange(0,20), (item) => {
-                Console.WriteLine(item.uniquename + ":" + item.ItemInfo.tier);
+            Console.WriteLine("Retrieving...");
+            Dictionary<String,String> test = new Dictionary<String, String>();
+            var items = AlbionOnlineDB.Items.GetItems().Result;
+            Parallel.ForEach(items, (item) => {
+                if(test.ContainsKey(item.ItemInfo.shopcategory))
+                {
+
+                }
+                else
+                {
+                    test.Add(item.ItemInfo.shopcategory, item.ItemInfo.JsonSource);
+                    Console.WriteLine(item.ItemInfo.shopcategory);
+                }
             });
-            Console.WriteLine("Test2");
-            Parallel.ForEach(AlbionOnlineDB.Items.GetItems().Result.GetRange(20, 20), (item) => {
-                Console.WriteLine(item.uniquename + ":" + item.ItemInfo.tier);
-            });
-            Console.WriteLine("Test3");
+
+            Console.WriteLine("Done. Re-Dumping");
+
+            foreach(var cat in test)
+            {
+                Console.WriteLine(cat.Key);
+                Console.WriteLine(cat.Value);
+            }
+
+            Console.WriteLine("Done. Press any key to exit.");
 
             Console.ReadKey();
         }
